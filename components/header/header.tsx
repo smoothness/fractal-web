58b77d0de455769e5a601a39328135fa5d6a68cc
Mauricio Poveda <mauricio.poveda@gmail.com>
2021-06-30 19:13:58 -0600

    First commit

---
+++ components/header/header.tsx
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+// import Link from 'next/link'
+
+// import styles from './header.module.css'
+
+function Header(): ReactElement {
+  return (
+    <header>
+      header
+    </header> 
+  )
+}
+
+export default Header
